e48affc1fda69d2876259e111edbffed79b8e4d0
Mohammad Shbool <alshboolmoh@gmail.com>
2020-11-03 17:40:39 +0200

    hide InterestedBy card

--- src/package/components/cards/cards.jsx
+++ src/package/components/cards/cards.jsx
@@ -4,7 +4,6 @@ import { createUseStyles, useTheme } from 'react-jss';
 
 import { BasicsCard } from './cards_types/basics/basics_card';
 import { ProjectsCard } from './cards_types/projects/projects_card';
-import { InterestedByCard } from './cards_types/interested_by/interested_by_card';
 import { SoundtrackCard } from './cards_types/soundtrack/soundtrack_card';
 import { StudiesCard } from './cards_types/studies/studies_card';
 import { ExperiencesCard } from './cards_types/experiences/experiences_card';
@@ -30,8 +29,7 @@ const CARD_TYPE_MAPPING = {
     experiences: ExperiencesCard,
     studies: StudiesCard,
     skills: SkillsCard,
-    soundtrack: SoundtrackCard,
-    interestedBy: InterestedByCard
+    soundtrack: SoundtrackCard
 };
 
 const CardsComponent = ({ cardsOrder = DEFAULT_CARD_ORDER, side, maxCardsPerRow }) => {
