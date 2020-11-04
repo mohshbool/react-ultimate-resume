5bc73d41cfa172f36d308f81178f1def8185c668
Mohammad Shbool <alshboolmoh@gmail.com>
2020-11-04 16:38:30 +0200

    remove LinkedIN share

--- src/package/components/footer/share_links/share_links_data.jsx
+++ src/package/components/footer/share_links/share_links_data.jsx
@@ -5,7 +5,6 @@ import { CopyWrapper } from '../../commons/copy_wrapper/copy_wrapper';
 
 import { ReactComponent as TwitterIcon } from '../../../assets/icons/brands/twitter.svg';
 import { ReactComponent as FacebookIcon } from '../../../assets/icons/brands/facebook.svg';
-import { ReactComponent as LinkedInIcon } from '../../../assets/icons/brands/linkedin.svg';
 import { ReactComponent as ShareIcon } from '../../../assets/icons/share.svg';
 
 export const SHARE_LINKS_DATA = Object.freeze({
@@ -30,17 +29,6 @@ export const SHARE_LINKS_DATA = Object.freeze({
             />
         ),
         getLink: ({ link }) => `https://www.facebook.com/sharer/sharer.php?u=${encodeURI(link)}`
-    },
-    linkedIn: {
-        icon: LinkedInIcon,
-        tooltipTranslation: (
-            <FormattedMessage
-                id="Footer.shareLinks.shareOnTooltip"
-                defaultMessage="Share on {platform}"
-                values={{ platform: 'LinkedIn' }}
-            />
-        ),
-        getLink: ({ link }) => `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURI(link)}`
     },
     copyShareUrl: {
         icon: (props) => (
